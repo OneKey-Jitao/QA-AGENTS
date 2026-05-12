6d7730180c461b7e37df821c484900832b9a8859
OneKey-Jitao <jitao.liang@onekey.so>
2026-05-12 20:13:31 +0800

    Adjust file directory

--- mobile-test/pages/general/index.js
+++ mobile-test/pages/general/index.js
@@ -1,3 +1,8 @@
+export * from './devPage.js';
+export * from './morePage.js';
+export * from './scanPage.js';
+export * from './setPasswordPage.js';
+
 export * from './menuPage.js';
 export * from './networkSelectPage.js';
 export * from './networkSettingsPage.js';
--- mobile-test/pages/index.js
+++ mobile-test/pages/index.js
@@ -19,14 +19,9 @@ export * from './modal/networkSelectorModalPage.js';
 
 export * from './general/index.js';
 
-export * from './homePage.js';
+export * from './homepage/homePage.js';
 export { default as CopyAddressPage } from './homepage/copyAddressPage.js';
 export * from './general/bottomNavBarPage.js';
-export * from './devPage.js';
-export * from './setPasswordPage.js';
-
-export * from './scanPage.js';
-export * from './morePage.js';
 
 export * from './swap/swapPage.js';
 export * from './swap/swapProPage.js';
